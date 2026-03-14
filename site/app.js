fdf5c553a6e8c5ffe50b9683ae14aae7b8bdc17f
Hillary Lewandowski <hlewan@umich.edu>
2026-03-14 17:57:47 -0400

    feat: finalize the rough draft with loading animations, mobile menu, and a little extra polish

--- site/app.js
+++ site/app.js
@@ -1,10 +1,12 @@
 import Alpine from "alpinejs";
 import focus from "@alpinejs/focus";
+import intersect from "@alpinejs/intersect";
 
 window.Alpine = Alpine;
 
 // Plugins.
 Alpine.plugin(focus);
+Alpine.plugin(intersect);
 
 // Global store.
 Alpine.store("app", {
